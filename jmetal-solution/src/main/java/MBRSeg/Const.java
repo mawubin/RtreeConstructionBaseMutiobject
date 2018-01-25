921b35dfd088cbdeeaf6d435b6c55e118c6a9e0d
mawubin <mawubin417@163.com>
2018-01-25 10:44:59 +0800

    sss

--- jmetal-solution/src/main/java/MBRSeg/Const.java
+++ jmetal-solution/src/main/java/MBRSeg/Const.java
@@ -1,5 +1,9 @@
 package MBRSeg;
 
 public class Const {
-		public static Integer numberOfSpatialObjects=30;
-		public static Integer numberOfClass=3;}
+		public static Integer numberOfSpatialObjects=300;
+		public static Integer numberOfClass=5;
+		public static Integer MaxEvaluations=10000;
+		public static Integer PopulationSize=1000;
+		
+}
